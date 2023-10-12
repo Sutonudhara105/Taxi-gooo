5782d95901fe4affe0193d0302402e05b47b78aa
SUTONU DHARA <sutonudhara105@gmail.com>
2023-10-13 01:14:20 +0530

    .

--- app/api/search-address/create-intent/route.tsx
+++ app/api/search-address/create-intent/route.tsx
@@ -1,18 +1,18 @@
 import { NextResponse } from "next/server";
-import stripe from "stripe";
+import Stripe from "stripe";
 const stripe=new Stripe(process.env.STRIPE_SECRET_KEY!,{
     typescript:true,
-    appVersion:"2023-08-16"
+    apiVersion:"2023-08-16"
 })
 export async function POST(request:any){
     const data:any=await request.json();
     const amount=data.amount;
     try{
-        const paymentIntext=await stripe.paymentIntents.create({
+        const paymentIntent=await stripe.paymentIntents.create({
             amount:Number(amount)*100,
             currency:'USD'
         })
-        return NextResponse.json(paymentIntext.client_secret,{status:200})
+        return NextResponse.json(paymentIntent.client_secret,{status:200})
     }
     catch(error:any){
         return new NextResponse(error,{
